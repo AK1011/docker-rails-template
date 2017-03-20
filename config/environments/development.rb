7ab517edc4c6bd8def5bfb85049b0418a35fd25e
Austin Kidd <austin.kidd@workday.com>
2017-03-20 16:03:23 -0700

    Changing the ip whitelist for development environment to include 172.0.0.0/24, cause the docker images seem to be finicky about their own ips reaching themselves

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Just whitelist those stupid docker ips
+  config.web_console.whitelisted_ips = '172.0.0.0/24'
 end
